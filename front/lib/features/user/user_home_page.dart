db74697e8c7c3a1d08f535fb03407fa6e0c18ab2
aourzik <aourzik@gmail.com>
2026-06-10 16:59:33 +0200

    feat: increase report cards padding for larger appearance

--- front/lib/features/user/user_home_page.dart
+++ front/lib/features/user/user_home_page.dart
@@ -314,7 +314,7 @@ class _ReportCard extends StatelessWidget {
     }
 
     final Widget card = Container(
-      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 17),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
       decoration: BoxDecoration(
         color: bgColor,
         borderRadius: BorderRadius.circular(28),
